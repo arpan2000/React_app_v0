e8d67c64a74c4548f77d0d83ee0aa18e243a6ac7
arpan2000 <itsallaboutak@gmail.com>
2023-12-08 21:10:36 +0530

    added codes for dynamic styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,26 @@ import { TagButton } from "./components/TagButton.jsx";
 // function are components in React
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState()
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton)
+  }
+
+  let tabContent = <p>Please select a tab</p>
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+    );
   }
 
   return (
@@ -22,30 +38,25 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
+            {/* <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} /> */}
             {/* using JS property of spread */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+            
+            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem}/>))}
+
           </ul>
         </section>
 
         <section id="examples">
           <menu>
-            <TagButton onSelect={() => handleSelect('components')}>Components</TagButton>
-            <TagButton onSelect={() => handleSelect('jsx')}>JSX</TagButton>
-            <TagButton onSelect={() => handleSelect('props')}>Props</TagButton>
-            <TagButton onSelect={() => handleSelect('state')}>State</TagButton>
+            <TagButton isSelected = {selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TagButton>
+            <TagButton isSelected = {selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TagButton>
+            <TagButton isSelected = {selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TagButton>
+            <TagButton isSelected = {selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TagButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TagButton.jsx
+++ src/components/TagButton.jsx
@@ -1,7 +1,7 @@
-export function TagButton ({children, onSelect}) {
+export function TagButton ({children, onSelect, isSelected}) {
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className = {isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 }
